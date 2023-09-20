7abdc270b0793a7236fa4f0ff5a3052647c7cded
fiqihalfito <fiqihalfito.dev@gmail.com>
2023-09-20 23:03:53 +0700

    minor

--- src/app/components/SocialLinkFooter.tsx
+++ src/app/components/SocialLinkFooter.tsx
@@ -10,6 +10,7 @@ export function SocialLinkFooter({ Icon, ...props }: SocialLinkFooterProp) {
     return (
         <Link
             aria-label="sosmed-icon"
+            title="sosmed-icon"
             {...props}
             className="group rounded-full  p-2 transition-all duration-200 hover:scale-110 hover:bg-white"
         >
